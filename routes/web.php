76b731f81f266eb31304132012b8afe23ffefe47
zazasyach <164740232+zazasyach@users.noreply.github.com>
2024-05-05 16:31:11 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\BarangController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WelcomeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $pageTitle = 'welcome';
+    return view('welcome', compact('pageTitle'));
+});
+
+
+Route::get('home', [HomeController::class, 'index'])->name('home');
+Route::get('welcome', [WelcomeController::class, 'index'])->name('welcome');
+
+Route::resource('barang', BarangController::class);
